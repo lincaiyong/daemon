9e5251db7e03392bef5c1a60b8594d611ca2d171
lincaiyong <lincaiyong@bytedance.com>
2025-10-09 16:55:07 +0800

    fix make

--- internal/exec.go
+++ internal/exec.go
@@ -2,34 +2,35 @@ package internal
 
 import (
 	"bufio"
-	"bytes"
+	"fmt"
 	"github.com/lincaiyong/log"
 	"os/exec"
 	"strings"
 	"sync"
 )
 
-func RunCommand(workDir, cmdName string, cmdArgs ...string) (string, error) {
+func RunCommand(workDir, cmdName string, cmdArgs ...string) error {
 	log.InfoLog("run command: %s %s", cmdName, strings.Join(cmdArgs, " "))
 	cmd := exec.Command(cmdName, cmdArgs...)
 	cmd.Dir = workDir
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.ErrorLog("fail to get stdout pipe: %v", err)
-		return "", err
+		return err
 	}
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		log.ErrorLog("fail to get stderr pipe: %v", err)
-		return "", err
+		return err
 	}
 	if err = cmd.Start(); err != nil {
 		log.ErrorLog("fail to start command: %v", err)
-		return "", err
+		return err
 	}
 
 	var wg sync.WaitGroup
-	var stdoutBuf, stderrBuf bytes.Buffer
+	//var stdoutBuf, stderrBuf bytes.Buffer
+	errChan := make(chan error, 2)
 
 	wg.Add(1)
 	go func() {
@@ -37,8 +38,12 @@ func RunCommand(workDir, cmdName string, cmdArgs ...string) (string, error) {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
 			line := scanner.Text()
-			stdoutBuf.WriteString(line + "\n")
-			log.InfoLog("%s", line)
+			//stdoutBuf.WriteString(line + "\n")
+			log.InfoLog("[STDOUT] %s", line)
+		}
+		if err = scanner.Err(); err != nil {
+			errChan <- err
+			log.ErrorLog("error reading stdout: %v", err)
 		}
 	}()
 	wg.Add(1)
@@ -47,15 +52,32 @@ func RunCommand(workDir, cmdName string, cmdArgs ...string) (string, error) {
 		scanner := bufio.NewScanner(stderr)
 		for scanner.Scan() {
 			line := scanner.Text()
-			stderrBuf.WriteString(line + "\n")
-			log.InfoLog("stderr: %s", line)
+			//stderrBuf.WriteString(line + "\n")
+			log.ErrorLog("[STDERR] %s", line)
+		}
+		if err = scanner.Err(); err != nil {
+			errChan <- err
+			log.ErrorLog("error reading stderr: %v", err)
 		}
 	}()
 	wg.Wait()
+
+	close(errChan)
+	for err = range errChan {
+		if err != nil {
+			return err
+		}
+	}
+
 	if err = cmd.Wait(); err != nil {
 		log.ErrorLog("fail to wait command: %v", err)
-		return "", err
+		return err
 	}
-	log.InfoLog("exit code: %d", cmd.ProcessState.ExitCode())
-	return "", nil
+	exitCode := cmd.ProcessState.ExitCode()
+	if exitCode != 0 {
+		log.ErrorLog("command failed with exit code: %d", exitCode)
+		return fmt.Errorf("command failed with exit code: %d", exitCode)
+	}
+	log.InfoLog("command completed successfully")
+	return nil
 }
